7c9b86819f6df399374c60a494e96f706518b3d2
muhlhauserneusch754 <muhlhauserneusch754@gmail.com>
2025-03-10 17:26:19 +0000

    Add 5.cpp

---
+++ src/5.cpp
@@ -0,0 +1,7 @@
+// This is an example of a simple C++ program that prints "Hello, World!" to the screen.
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
