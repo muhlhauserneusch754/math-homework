29b1b91edb09d0f2ad2e07f4e3bb126133c1f087
muhlhauserneusch754 <muhlhauserneusch754@gmail.com>
2025-03-11 06:19:14 +0000

    Add 6.cpp

---
+++ src/6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    int z = 15;
+    cout << "The sum of " << x << " and " << y << " is " << x + y << endl;
+    cout << "The difference between " << x << " and " << z << " is " << x - z << endl;
+    return 0;
+}
