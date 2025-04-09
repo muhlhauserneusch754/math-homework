0a2349381e09cd8337abb649361afb0923eba7a9
muhlhauserneusch754 <muhlhauserneusch754@gmail.com>
2025-04-09 09:53:46 +0000

    Add 27.cpp

---
+++ src/27.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "The number is positive." << endl;
+    } else if (x == 0) {
+        cout << "The number is zero." << endl;
+    } else {
+        cout << "The number is negative." << endl;
+    }
+    return 0;
+}
